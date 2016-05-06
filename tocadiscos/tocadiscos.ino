7c48d19fa26eb400d300e07878957d5317d0d987
codeneomatrix <josuecevedo@gmail.com>
2016-05-05 21:11:39 -0500

    leyendo los tiempos de rotacion del disco

--- tocadiscos/tocadiscos.ino
+++ tocadiscos/tocadiscos.ino
@@ -1,25 +1,73 @@
 int led = 12;
-
+long start=0;
 int prueba;
+
 void setup() {  
   Serial.begin(9600);  
   pinMode(led, OUTPUT);     
-//  pinMode(foto, INPUT);       
+  pinMode(2, INPUT);
+  digitalWrite(2,HIGH);
+  digitalWrite(led, HIGH);
+  
+  randomSeed(millis());
+  delay(random(1000,3000));
+  attachInterrupt(0,react,RISING); 
+  start= millis();
 }
 
 
 void loop() {
   
   prueba = analogRead(A0);
-  Serial.print(prueba);
-  Serial.print("\n");  
-  
+  //Serial.print(analogRead(A0));
+  //Serial.print("\n");  
   //if(prueba>=90){//valor del sesor del plato
-  if(prueba>=10){
-    digitalWrite(led, HIGH);
+  if(prueba>=135){
+    digitalWrite(led, LOW);
   }else{
-    digitalWrite(led, LOW); 
+    digitalWrite(led, HIGH);
   }
   
+}
+
+void react(){
+  long fin= millis();
+  //detachInterrupt(0);
+  //digitalWrite(led, HIGH);
+  Serial.print("tiempo ");
+  Serial.print(fin-start);
+  Serial.print("\n"); 
+  if((fin-start)==1278){
+    Serial.print("33 rv"); 
+    Serial.print("\n"); 
+  }
+  if((fin-start)==1279){
+    Serial.print("33 rv"); 
+    Serial.print("\n"); 
+  }
+  if((fin-start)==1280){
+    Serial.print("33 rv"); 
+    Serial.print("\n"); 
+  }
+  if((fin-start)==1742){
+    Serial.print("45 rv"); 
+    Serial.print("\n"); 
+  }
+  if((fin-start)==1743){
+    Serial.print("45 rv"); 
+    Serial.print("\n"); 
+  }
+  if((fin-start)==1744){
+    Serial.print("45 rv"); 
+    Serial.print("\n"); 
+  }
+  if((fin-start)==1745){
+    Serial.print("45 rv"); 
+    Serial.print("\n"); 
+  }
+   
+  
+  start= millis();
   
 }
+
