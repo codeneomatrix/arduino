2cd54fec761cd0cdb4668474d93b51d7d84a3d82
codeneomatrix <josuecevedo@gmail.com>
2016-05-09 00:25:28 -0500

    pwm y tamaño del disco

--- tocadiscos/tocadiscos.ino
+++ tocadiscos/tocadiscos.ino
@@ -1,13 +1,16 @@
 int led = 12;
+int motor = 10;
 long start=0;
-int prueba;
-
+boolean chico=false;
+int veces=1;
 void setup() {  
   Serial.begin(9600);  
   pinMode(led, OUTPUT);     
   pinMode(2, INPUT);
+  pinMode(motor, OUTPUT);
+  
   digitalWrite(2,HIGH);
-  digitalWrite(led, HIGH);
+  digitalWrite(led, LOW);
   
   randomSeed(millis());
   delay(random(1000,3000));
@@ -18,56 +21,57 @@ void setup() {
 
 void loop() {
   
-  prueba = analogRead(A0);
-  //Serial.print(analogRead(A0));
-  //Serial.print("\n");  
-  //if(prueba>=90){//valor del sesor del plato
-  if(prueba>=135){
-    digitalWrite(led, LOW);
-  }else{
-    digitalWrite(led, HIGH);
+  //prueba = analogRead(A0);
+  /*Serial.print(digitalRead(2));
+  Serial.print("\n");
+  Serial.print(analogRead(A0)/4);
+  Serial.print("\n");  */
+  
+  if(analogRead(A0)<20){
+    chico= true;
   }
+  analogWrite(motor, analogRead(A0)/4);
+  /*if(chico){
+    Serial.print("va a 45 rv");
+    Serial.print("\n");
+  }else{
+    Serial.print("va a 33 rv");
+    Serial.print("\n");
+  }*/
+  
   
 }
 
 void react(){
   long fin= millis();
+  if(fin-start>=500){
+ 
   //detachInterrupt(0);
   //digitalWrite(led, HIGH);
-  Serial.print("tiempo ");
+  //Serial.print("tiempo ");
   Serial.print(fin-start);
+  Serial.print("-->");
+  Serial.print( analogRead(A0)/4);
   Serial.print("\n"); 
-  if((fin-start)==1278){
+  if(((fin-start)>=1275)&&((fin-start)<=1280)){
     Serial.print("33 rv"); 
     Serial.print("\n"); 
+    digitalWrite(led, HIGH);
   }
-  if((fin-start)==1279){
-    Serial.print("33 rv"); 
-    Serial.print("\n"); 
-  }
-  if((fin-start)==1280){
-    Serial.print("33 rv"); 
-    Serial.print("\n"); 
-  }
-  if((fin-start)==1742){
+  if(((fin-start)>=1740)&&((fin-start)<=1745)){
     Serial.print("45 rv"); 
     Serial.print("\n"); 
-  }
-  if((fin-start)==1743){
-    Serial.print("45 rv"); 
-    Serial.print("\n"); 
-  }
-  if((fin-start)==1744){
-    Serial.print("45 rv"); 
-    Serial.print("\n"); 
-  }
-  if((fin-start)==1745){
-    Serial.print("45 rv"); 
-    Serial.print("\n"); 
-  }
+    digitalWrite(led, HIGH);
+  }else{
+    digitalWrite(led, LOW);
+    }
    
   
   start= millis();
+  
+  
+  }
+  
   
 }
 
